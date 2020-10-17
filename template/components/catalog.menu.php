a1b47975da70a5570a3bc62379c8d3d4d38f9564
Artem Bichurin <bichurinet@ya.ru>
2020-10-17 12:13:27 +0500

    add design in menu (#11)

--- template/components/catalog.menu.php
+++ template/components/catalog.menu.php
@@ -1,8 +1,12 @@
-<nav class="catalog-menu">
-    <ul class="catalog-menu__inner">
+<nav class="menu catalog__menu">
+    <ul class="menu__inner">
         <?foreach($arrComponentList as $key => $item):?>
-            <li class="catalog-menu__item">
-                <a href="/?component=<?=$key?>" class="catalog-menu__link"><?=$item['name']?></a>
+            <li class="menu__item">
+                <?if ($key === $_GET['component']):?>
+                    <a href="/?component=<?=$key?>" class="menu__link menu__link_active"><?=$item['name']?></a>
+                <?else:?>
+                    <a href="/?component=<?=$key?>" class="menu__link menu__link"><?=$item['name']?></a>
+                <?endif?>
             </li>
         <?endforeach?>
     </ul>
